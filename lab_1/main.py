8f8b8c835143d6badf0566ef4edae56448905c7a
syth0le <chdan565@gmail.com>
2022-02-12 16:07:07 +0300

    added some fixes in algorithm

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,13 +1,32 @@
 from perceptron import Perceptron
 from utils.reader import Reader
 
+NU = 000.1
+H = 0.7
+
 
 def main():
-    data = Reader.read_data()
-    perceptron = Perceptron(size=10)
-    perceptron.train(data, epochs=10, nu=0.7)
-    result = perceptron.predict(data)
-    print(f'This is output: {result}')
+    reader = Reader()
+    data = reader.read_train_data()
+    size = len(data[0])
+    layers = []
+    for i in range(10):
+        perceptron = Perceptron(size=size, i=i, h=H)
+        layers.append(perceptron)
+
+    for number in data:
+        print(f'Number: {number} in train data')
+        for i in range(10):
+            perceptron = layers[i]
+            perceptron.train(data=data[number], epochs=1000, nu=NU)
+            print(f'Perceptron: {i}, {perceptron.predict(data[number])}')
+
+    print('THE END OF TRAINING\n')
+
+    for perceptron in layers:
+        test_data = reader.read_test_data()
+        result = perceptron.predict(data=test_data)
+        print(f'This is output for {perceptron}: {result}')
 
 
 if __name__ == '__main__':
--- lab_1/perceptron.py
+++ lab_1/perceptron.py
@@ -5,21 +5,26 @@ import numpy as np
 
 class Perceptron:
 
-    def __init__(self, size: int):
+    def __init__(self, size: int, i: int = 0, h: float = 0.75) -> None:
+        self.i = i
+        self.h = h
         self.weights = self._get_weights(size)
 
-    def train(self, data: List[float], nu: float, epochs: int = 1):
+    def train(self, data: List[float], nu: float = 1, epochs: int = 1) -> None:
+        target = self.__get_target_data(0)
         for epoch in range(epochs):
-            sum_local = self.__get_sum_for_weight(data)
+            sum_local = self.__get_sum_for_weight(data) + ((-1) * self.h)
             res = self.__get_activation_function(sum_local)
-            error = self.__get_error(res)
+            error = self.__get_error(target, res)
             self.__normalize_weights(data, nu, error)
+            self.h += error * (-1) * nu
+            print(f'>epoch={epoch}, lrate={nu}')
 
     def fit(self):
         pass
 
     def predict(self, data: List[float]):
-        sum_local = self.__get_sum_for_weight(data)
+        sum_local = self.__get_sum_for_weight(data) - self.h
         return self.__get_activation_function(sum_local)
 
     @staticmethod
@@ -27,19 +32,27 @@ class Perceptron:
         return np.random.uniform(-1, 1, size)
 
     @staticmethod
-    def __get_activation_function(sum: float) -> int:
-        return 1 if sum > 0 else 0
+    def __get_activation_function(sum_local: float) -> int:
+        return 1 if sum_local > 0 else 0
 
     def __get_sum_for_weight(self, data: List[float]) -> float:
-        res_array = []
-        for i in range(len(data)):
-            res_array.append(data[i] * self.weights[i])
-        return sum(res_array)
+        net_y = 0
+        length = len(data)
+        if length != len(self.weights):
+            raise Exception
+        for i in range(length):
+            net_y += data[i] * self.weights[i]
+        return net_y
 
     @staticmethod
-    def __get_error(res: float) -> float:
-        return self.number - res
+    def __get_error(target: float, res: float) -> float:
+        return target - res
 
     def __normalize_weights(self, data: List[float],  nu: float, error: float) -> None:
         for i, _ in enumerate(self.weights):
             self.weights[i] = error * nu * data[i]
+
+    @staticmethod
+    def __get_target_data(number: int) -> float:
+        return 0
+

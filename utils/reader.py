18f64402b945572f513208d2b1dffb5470a2b65a
syth0le <chdan565@gmail.com>
2022-02-12 16:10:12 +0300

    added skelet for class Reader

--- utils/reader.py
+++ utils/reader.py
@@ -1,3 +1,10 @@
+from typing import List
+
+
 class Reader:
-    def read_data():
+
+    def read_train_data(self) -> List[List[float]]:
+        pass
+
+    def read_test_data(self) -> List[List[float]]:
         pass
